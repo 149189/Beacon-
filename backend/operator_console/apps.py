96a65f463d64eed1f671596a2c3c1395ec8a630a
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-08-14 19:41:48 +0530

    changeing everything

--- backend/operator_console/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class OperatorConsoleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'operator_console'
-    verbose_name = 'Operator Console'
